26a4532f85f91342440a07860607c0d792b85103
fretgfr <fretgfr@sudomail.com>
2023-09-27 20:40:48 -0400

    Small changes

--- zipline/errors.py
+++ zipline/errors.py
@@ -19,7 +19,15 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
-__all__ = ("ZiplineError", "UnhandledError", "BadRequest", "Forbidden", "NotFound", "ServerError", "NotAuthenticated")
+__all__ = (
+    "ZiplineError",
+    "UnhandledError",
+    "BadRequest",
+    "Forbidden",
+    "NotFound",
+    "ServerError",
+    "NotAuthenticated",
+)
 
 
 class ZiplineError(Exception):

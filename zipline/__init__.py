e73704bcb0ef38ad95b540c373057beb992f3bbb
fretgfr <51489753+fretgfr@users.noreply.github.com>
2024-01-25 18:11:25 -0500

    Update copyright text.

--- zipline/__init__.py
+++ zipline/__init__.py
@@ -27,5 +27,5 @@ from .models import *
 __title__ = "zipline.py"
 __author__ = "fretgfr"
 __license__ = "MIT"
-__copyright__ = "Copyright 2023 fretgfr"
+__copyright__ = "Copyright 2023-present fretgfr"
 __version__ = "0.11.0"

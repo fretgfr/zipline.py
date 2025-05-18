e14f861c2890d8bab49cd6e23791c0f1ae0f4d6f
cswimr <seaswimmerthefsh@gmail.com>
2025-05-17 20:30:31 -0500

    add the shorten command

--- zipline/cli/commands/__init__.py
+++ zipline/cli/commands/__init__.py
@@ -1,5 +1,6 @@
 from ..asynctyper import AsyncTyper
+from .shorten import app as shorten
 from .upload import app as upload
 from .version import app as version
 
-commands: list[AsyncTyper] = [upload, version]
+commands: list[AsyncTyper] = [shorten, upload, version]
---
+++ zipline/cli/commands/shorten.py
@@ -0,0 +1,103 @@
+from typing import Annotated, Optional
+
+from aiohttp import NonHttpUrlClientError
+from rich.progress import Progress, SpinnerColumn, TextColumn
+from typer import Argument, Exit, Option
+
+from zipline.cli.asynctyper import AsyncTyper
+from zipline.client import Client
+from zipline.errors import Forbidden, NotAuthenticated
+
+app = AsyncTyper()
+
+
+@app.command(name="shorten")
+async def shorten(
+    original_url: Annotated[
+        str,
+        Argument(help="The url you wish to shorten."),
+    ],
+    instance: Annotated[
+        str,
+        Option(
+            "--instance",
+            "-i",
+            help="Specify the URL of your Zipline instance.",
+            envvar="ZIPLINE_INSTANCE",
+            prompt=True,
+        ),
+    ],
+    token: Annotated[
+        str,
+        Option(
+            "--token",
+            "-t",
+            help="Specify a token used for authentication against your chosen Zipline instance.",
+            envvar="ZIPLINE_TOKEN",
+            prompt=True,
+            hide_input=True,
+        ),
+    ],
+    vanity: Annotated[
+        Optional[str],
+        Option(
+            "--vanity",
+            "-v",
+            help="Specify a vanity name. A name will be generated automatically if this is not provided.",
+        ),
+    ] = None,
+    max_views: Annotated[
+        Optional[int],
+        Option(
+            "--max-views",
+            "-m",
+            help="Specify the number of times this url can be used before being deleted. No limit will be set if this is not provided.",
+        ),
+    ] = None,
+    password: Annotated[
+        Optional[str],
+        Option("--password", "-p", help="Specify a password required to use the url."),
+    ] = None,
+    enabled: Annotated[
+        bool,
+        Option(
+            "--enable/--disable",
+            "-e/-d",
+            help="Specify whether the url should be immediately usable. If the url is disabled, you will need to use the Zipline website to enable it manually.",
+        ),
+    ] = True,
+) -> None:
+    """Upload a file to a remote Zipline instance."""
+    with Progress(
+        SpinnerColumn(),
+        TextColumn("[progress.description]{task.description}"),
+        transient=True,
+    ) as progress:
+        task = progress.add_task(description="Preparing client...", total=None)
+        client = Client(instance, token)
+
+        progress.update(task, description="Creating shortened url...", total=None)
+        try:
+            shortened_url = await client.shorten_url(
+                original_url=original_url,
+                vanity=vanity,
+                max_views=max_views,
+                password=password,
+                enabled=enabled,
+            )
+        except NonHttpUrlClientError as e:
+            print(f"Invalid URL provided: '{instance}'")
+            await client.close()
+            raise Exit(1) from e
+        except (NotAuthenticated, Forbidden) as e:
+            print("Authentication failure! Are you using a valid token?")
+            await client.close()
+            raise Exit(77) from e
+        # except BadRequest as e:
+        #     print("Bad request!")
+        #     await client.close()
+        #     raise Exit(1) from e
+
+        await client.close()
+
+    print(shortened_url)

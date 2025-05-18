d2ea591d8f2ed6f94da876a1f29d1cc4e4c622cb
cswimr <seaswimmerthefsh@gmail.com>
2025-05-17 20:05:52 -0500

    finish the upload command

--- zipline/cli/commands/upload.py
+++ zipline/cli/commands/upload.py
@@ -1,13 +1,15 @@
 from datetime import datetime
-from typing import Annotated
+from typing import Annotated, Optional
 
-import typer
+from aiohttp import NonHttpUrlClientError
+from rich.progress import Progress, SpinnerColumn, TextColumn
+from typer import Argument, Exit, FileBinaryRead, Option
 
 from zipline.cli.asynctyper import AsyncTyper
 from zipline.client import Client
 from zipline.enums import NameFormat
 from zipline.errors import BadRequest, Forbidden, NotAuthenticated
-from zipline.models import FileData
+from zipline.models import FileData, UploadResponse
 
 app = AsyncTyper()
 
@@ -15,12 +17,12 @@ app = AsyncTyper()
 @app.command(name="upload")
 async def upload(
     file: Annotated[
-        typer.FileBinaryRead,
-        typer.Argument(help="The path to the file you wish to upload."),
+        FileBinaryRead,
+        Argument(help="The path to the file you wish to upload."),
     ],
     instance: Annotated[
         str,
-        typer.Option(
+        Option(
             "--instance",
             "-i",
             help="Specify the URL of the Zipline instance you're uploading to.",
@@ -30,7 +32,7 @@ async def upload(
     ],
     token: Annotated[
         str,
-        typer.Option(
+        Option(
             "--token",
             "-t",
             help="Specify a token used for authentication against your chosen Zipline instance.",
@@ -41,37 +43,132 @@ async def upload(
     ],
     format: Annotated[
         NameFormat,
-        typer.Option(
+        Option(
             "--format",
             "-f",
             help="Specify what format Zipline should use to generate a link for this file.",
         ),
     ] = NameFormat.uuid,
+    compression_percent: Annotated[
+        int,
+        Option(
+            "--compression-percent",
+            "-c",
+            help="Specify how much this file should be compressed.",
+        ),
+    ] = 0,
     expiry: Annotated[
-        datetime | None,
-        typer.Option(
+        Optional[datetime],
+        Option(
             "--expiry",
             "-e",
             help="Specify when this file should expire. When this time expires, the file will be deleted from the Zipline instance.",
         ),
     ] = None,
+    password: Annotated[
+        Optional[str],
+        Option(
+            "--password",
+            "-p",
+            help="Specify a password for this file. Viewing this file from Zipline will then require having this password.",
+        ),
+    ] = None,
+    max_views: Annotated[
+        Optional[int],
+        Option(
+            "--max-views",
+            "-m",
+            help="Specify how many times this file can be viewed before it will be automatically deleted.",
+        ),
+    ] = None,
+    override_name: Annotated[
+        Optional[str],
+        Option(
+            "--name",
+            "-n",
+            help="Specify the name to give this file in Zipline. This overrides the --format option, if provided.",
+        ),
+    ] = None,
+    original_name: Annotated[
+        Optional[bool],
+        Option(
+            "--original-name/--generated-name",
+            "-o/-g",
+            help="Specify whether the original name of the file should be preserved when downloading the file from Zipline.",
+        ),
+    ] = False,
+    folder: Annotated[
+        Optional[str],
+        Option(
+            "--folder",
+            "-f",
+            help="Specify what folder the file should be added to after it is uploaded.",
+        ),
+    ] = None,
+    override_extension: Annotated[
+        Optional[str],
+        Option(
+            "--extension",
+            "-e",
+            help="Specify what file extension the file should be uploaded with. This should only really be used if the library fails to determine what kind of file your file is.",
+        ),
+    ] = None,
+    override_domain: Annotated[
+        Optional[str],
+        Option(
+            "--domain",
+            "-d",
+            help="Specify what domain should be used instead of the Zipline instance's core domain.",
+        ),
+    ] = None,
 ) -> None:
     """Upload a file to a remote Zipline instance."""
-    client = Client(instance, token)
-    file_data = FileData(file)
+    with Progress(
+        SpinnerColumn(),
+        TextColumn("[progress.description]{task.description}"),
+        transient=True,
+    ) as progress:
+        task = progress.add_task(description="Preparing client...", total=None)
+        client = Client(instance, token)
 
-    try:
-        uploaded_file = await client.upload_file(
-            payload=file_data, expiry=expiry, format=format
+        progress.update(task, description="Reading file...", total=None)
+        file_data = FileData(
+            data=file,
+            filename=override_name or file.name,
         )
-    except (NotAuthenticated, Forbidden) as e:
-        print("Authentication failure! Are you using a valid token?")
-        await client.close()
-        raise typer.Exit(77) from e
-    except BadRequest as e:
-        print("Bad request!")
-        await client.close()
-        raise typer.Exit(1) from e
 
-    print(uploaded_file.files[0])
-    await client.close()
+        progress.update(task, description="Uploading file...", total=None)
+        try:
+            uploaded_file = await client.upload_file(
+                payload=file_data,
+                compression_percent=compression_percent,
+                expiry=expiry,
+                format=format,
+                password=password,
+                max_views=max_views,
+                override_name=override_name,
+                original_name=original_name,
+                folder=folder,
+                override_extension=override_extension,
+                override_domain=override_domain,
+                text_only=True,
+            )
+        except NonHttpUrlClientError as e:
+            print(f"Invalid URL provided: '{instance}'")
+            await client.close()
+            raise Exit(1) from e
+        except (NotAuthenticated, Forbidden) as e:
+            print("Authentication failure! Are you using a valid token?")
+            await client.close()
+            raise Exit(77) from e
+        except BadRequest as e:
+            print("Bad request!")
+            await client.close()
+            raise Exit(1) from e
+
+        await client.close()
+
+    if isinstance(uploaded_file, UploadResponse):
+        print(uploaded_file.files[0])
+    else:
+        print(uploaded_file)

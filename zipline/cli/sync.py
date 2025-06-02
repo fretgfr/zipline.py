2025051693ee05f2876eb496fbeb3f448548cfc2
cswimr <seaswimmerthefsh@gmail.com>
2025-06-02 15:19:02 -0500

    typehint the `@sync` decorator properly

--- zipline/cli/sync.py
+++ zipline/cli/sync.py
@@ -1,10 +1,24 @@
 from asyncio import run
 from functools import wraps
+from typing import Any, Callable, Coroutine, TypeVar
+
+from typing_extensions import ParamSpec
+
+Params = ParamSpec("Params")
+Response = TypeVar("Response")
 
 
-def sync(func):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
+def sync(
+    func: Callable[Params, Coroutine[Any, Any, Response]],
+) -> Callable[Params, Response]:  # pyright: ignore[reportExplicitAny]
+    """
+    Decorator that takes an async function `func(...) -> Coroutine[Any, Any, Response]`
+    and turns it into a synchronous function `() -> Response` by running
+    `asyncio.run(...)` under the hood.
+    """
+
+    @wraps(wrapped=func)
+    def wrapper(*args: Params.args, **kwargs: Params.kwargs) -> Response:
         return run(func(*args, **kwargs))
 
     return wrapper
